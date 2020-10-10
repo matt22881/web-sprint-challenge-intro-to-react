158fecba0f97103fcb1f4155cbfe889dc33d9bfe
your_username <54979583+BigStem420@users.noreply.github.com>
2020-10-10 17:19:11 -0400

    starting over

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import './App.css';
 import {
   Collapse,
@@ -32,9 +32,9 @@ import {
   CarouselIndicators,
   TabContent,
   TabPane,
-  
+  ButtonGroup, ButtonDropdown,
   Card,
-   
+    IsOpen, 
   CardTitle, 
   CardText, 
   Row,
@@ -43,10 +43,12 @@ import {
 } from 'reactstrap';
 
 
+// class App extends Component {
 
 const App = () => {
 
-  //! TODO const [ myState, setMyState() ] = setMyState(null) 
+//   const [ myState, setMyState ] = useState(null) 
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -54,46 +56,39 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+//   const [dropdownOpen, setOpen] = useState(false);
+
+//   const toggle = () => setOpen(!dropdownOpen);
+
+//   const Example = (props) => {
+
   return (
     <div className="App">
+      <div className='full'>
+        <Jumbotron>
+            <h1 className="display-3">Hello, world!</h1>
+            <p className="lead">top p</p>
+            <hr className="my-2" />
+            <p>another p</p>
+            <p className="lead">
+            <Button color="primary">Learn More</Button>
+            </p>
+        </Jumbotron>
 
-  <Nav pills>
-        <NavItem>
-          <NavLink href="#" active>Link</NavLink>
-        </NavItem>
-        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle nav caret>
-            Dropdown
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem header>Header</DropdownItem>
-            <DropdownItem disabled>Action</DropdownItem>
-            <DropdownItem>Another Action</DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>Another Action</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <NavItem>
-          <NavLink href="#">Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Another Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Disabled Link</NavLink>
-        </NavItem>
-      </Nav>
+    
+            <div className='left-side'>
+               
+            </div>
+            
+            <div className='right-side'>
 
-      <Jumbotron>
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
-        </p>
-      </Jumbotron>
+            </div>
+        </div>
+    
 
+
+      
 
 
 
@@ -107,5 +102,6 @@ const App = () => {
     </div>
   );
 }
+
 
 export default App;
