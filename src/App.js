a3d2015e7c5ba4f7daaa0d27a1d7a07c77530f22
matt22881 <matt22881@gmail.com>
2020-11-06 18:42:50 -0500

    nearly happy with it now

--- src/App.js
+++ src/App.js
@@ -1,19 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
+import styled from 'styled-components'
+
+const app = styled.div`
+    display: flex;
+    max-width: 40vw;
+    border: 2px dotted brown;
+    padding 2 rem;
+  `
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+
+  const [characters, setCharacters] = useState([])
+  const [episodes, setEpisodes] = useState([])
+  const [locations, setLocations] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+    useEffect(() => {
+        axios
+            .get('http://localhost:4242/api/character')
+            .then(res => {
+                setCharacters(Array.from(res.data)
+            )})
+            .catch(err => {
+                console.log('!!ERR!! (get #1): ', err)
+                // alert(`Something ain't right here, bud =( (get#1)`)
+                // debugger
+            })
+    },[])
+
+    useEffect(() => {
+            axios
+                .get(`http://localhost:4242/api/episode`)
+                .then(res => {
+                    setEpisodes(Array.from(res.data))
+                })
+                .catch(err => {
+                    console.log('!!ERR!! (get #2): ', err)
+                    // alert(`Something ain't right here, bud =( (get#2)`)
+                    // debugger
+                })            
+    },[])
+
+    useEffect(() => {
+            axios
+                .get(`http://localhost:4242/api/location`)
+                .then(res => {
+                    setLocations(Array.from(res.data))
+                })
+                .catch(err => {
+                    console.log('!!ERR!! (get #3): ', err)
+                    // alert(`Something ain't right here, bud =( (get#3)`)
+                    // debugger
+                })
+    },[])
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+
+<div>
+<div className='filler'></div>
+    <div>
+        <app>
+            {
+            characters.map(character => {
+                return (<Character
+                    character={character}
+                    episodes={episodes}
+                    locations={locations}
+                    key={character.id}
+                    />)})
+            }
+        </app>
     </div>
+</div>
   );
 }
+
+
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,112 @@
 // Write your Character component here
+import React, { useState } from 'react'
+import Episode from './Episode'
+// import Location from './Location'
+import styled from 'styled-components'
+
+const Char =styled.div`
+    display: block;
+    border: 1px solid  #f9e7dc;
+    margin-left: 50%;
+    margin-bottom: 2rem;
+    width: 40vw;
+    background: rgba(0, 0, 0, 0.6);
+    text-align: center;
+    position: relative;
+
+`
+
+const Head = styled.h1`
+    display:block;
+    width: 100%;
+    margin: auto 0;
+    
+`
+
+const Epis = styled.ul`
+    columns: 2;
+`
+
+const ImgContainer = styled.div`
+    display: inline-block;
+    width: 75%;
+    img {
+        width: 75%;
+        }
+
+`
+const BodyContainer = styled.div`
+    color: #f9e7dc;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    padding: 1rem;
+`
+const ContentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto 0;
+`
+const ShowEpisodes = styled.button`
+    background-color: #f9e7dc;
+    display: inline-block;
+    border: 1px solid black;
+    width: 25%;
+    margin: 1rem auto;
+    border-radius: 5px;
+`
+
+export default function Character(props) {
+    const { name, url, status, species, image, episode, location, id  } = props.character
+
+    const [toggle, toggleToggle] = useState(false)
+    const onClick = (e) => {
+        toggleToggle(!toggle)
+    } 
+
+    return (
+        <Char> 
+            <BodyContainer>
+                <Head>  {/* eslint-disable-next-line*/}
+                    <a href={url} target='_blank'>{name}</a>
+                </Head>
+                <ContentContainer id='big'>
+                    <ImgContainer>
+                        <img src={image} alt={name} />
+                    </ImgContainer>
+                    <ContentContainer className='column'>
+                        <ContentContainer className='column'>
+                            <h2>Species:</h2>
+                            <p>{species}</p>
+                        </ContentContainer>
+                        <ContentContainer className='column'>
+                            <h2>Status:</h2>
+                            <p>{status}</p>
+                        </ContentContainer>
+                        <ContentContainer className='column'>
+                            <h3>Last Known Location:</h3>
+                            <a href={location.url}>{location.name}</a>
+                        </ContentContainer>
+                    </ContentContainer>
+                </ContentContainer>
+                    <ContentContainer className='row'>
+                        <h4>Want to see all their Episodes?</h4>
+                        <ShowEpisodes onClick={onClick}>{!toggle ? 'Show Me!' : 'Hide Them'}</ShowEpisodes>
+                    </ContentContainer>
+            </BodyContainer>
+
+                <Epis>
+                    {toggle ? 
+                        episode.map(epi => {
+                        return (<Episode
+                            episode={epi}
+                            episodes={props.episodes}
+                            key={episode.id}
+                        />)
+                        })
+                    : null}
+                </Epis>
+        </Char>
+    )
+}
