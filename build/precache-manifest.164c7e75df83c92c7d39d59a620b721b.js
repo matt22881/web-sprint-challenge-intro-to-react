7120c8dd208363b2cf1329ffcdd33b786df219b4
matt22881 <matt22881@gmail.com>
2020-11-06 19:47:13 -0500

    add build file

---
+++ build/precache-manifest.164c7e75df83c92c7d39d59a620b721b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc66193b2e3f74d27ed165b6a371dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6e7d1720b940d06223",
+    "url": "/static/css/main.45d1a319.chunk.css"
+  },
+  {
+    "revision": "f071d6044e6db5aec2b3",
+    "url": "/static/js/2.629916e7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.629916e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af6e7d1720b940d06223",
+    "url": "/static/js/main.1d50e490.chunk.js"
+  },
+  {
+    "revision": "57fca467b55bfed9c5ca",
+    "url": "/static/js/runtime-main.f688ac41.js"
+  },
+  {
+    "revision": "24fd4764e2585c51f24a3de5ea19521a",
+    "url": "/static/media/peace-among-worlds.24fd4764.jpg"
+  }
+]);
